fc1e1da85ffc642b85e55bb333fe6d9a7a1ca645
Deniz İnan Şimşek <denizinansimsek@outlook.com>
2023-07-13 17:50:05 +0300

    fix: code clean up

--- Hospital.Management/FrmPatientSignUp.cs
+++ Hospital.Management/FrmPatientSignUp.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace Hospital.Management
 {
@@ -31,7 +24,7 @@ namespace Hospital.Management
             command.Parameters.AddWithValue("@p6", CmbGender.Text);
             command.ExecuteNonQuery();
             connection1.Connection().Close();
-            MessageBox.Show("Your registration has been completed. Your password is: "+TxtPassword.Text, "Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            MessageBox.Show("Your registration has been completed. Your password is: " + TxtPassword.Text, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
